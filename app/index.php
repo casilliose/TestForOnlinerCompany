8e9983e29effff6503f343f448f3827bdc9f964d
Branitsky Igor <igor.branitsky.contr@ironmountain.com>
2021-06-26 16:23:51 +0300

    add redis cluster and composer

--- app/index.php
+++ app/index.php
@@ -1,2 +1,9 @@
 <?php
-echo 'start work';
+require __DIR__ . '/vendor/autoload.php';
+$parameters = ['tcp://redis-node-0', 'tcp://redis-node-1', 'tcp://redis-node-2'];
+$options = ['cluster' => 'redis'];
+
+$client = new Predis\Client($parameters, $options);
+$client->set('foo', 'bar');
+$value = $client->get('foo');
+var_dump($value);
---
+++ ci/php/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /www && composer install
+php-fpm
